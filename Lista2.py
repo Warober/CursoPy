21337e801de31e450f20f60b9e9278ca283e8e47
rober <robertorx50@gmail.com>
2019-09-24 18:30:45 +0200

    24/09/19

--- Lista2.py
+++ Lista2.py
@@ -1,20 +1,16 @@
 
 my_list = []
 inpunt_user = ""
-X=0
-
-
 
 while inpunt_user != "Fin" :
     inpunt_user = input("Introduce los elementos para añadir a la listade la compra: (Teclea Fin para Salir)")
     if inpunt_user != "FiN" :
         my_list.append(inpunt_user)
 
-longitud = len(my_list)
+for X in my_list:
+    print("Tengo que comprar {}".format(my_list))
 
 
-while longitud > X :
-    print(my_list[X])
-    X += 1
+
 
 
